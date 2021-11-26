8bfec14947d9b5298ffbabc027d24681ef4353b4
Tauheed Butt <tauheedbutt@gmail.com>
2021-11-26 20:08:47 +0500

    task_5.py UPDATED

--- Lab/Lab3/task_5.py
+++ Lab/Lab3/task_5.py
@@ -1,10 +1,25 @@
 import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
 
+# Data
 feature1 = [12, 11, 8, 6, 9, 6, 10]
 feature2 = [4, 5, 1, 4, 3, 6, 2]
+classes = ['a', 'a', 'a', 'b', 'b', 'a', 'b']
 
-plt.scatter(feature1, feature2)
+# Add Legend
+a = mlines.Line2D([], [], marker='o', linestyle='None',
+                          markersize=10, label='Class: a')
+b = mlines.Line2D([], [], marker='x', linestyle='None',
+                          markersize=10, label='Class: b')
+plt.legend(handles=[a,b])
 
-plt.colorbar(ticks=[0, 1])
+# Scatter the Graph
+for i in range(len(classes)):
+    marker='o'
+    if classes[i]=='b': 
+        marker='x'
+    print(f'{classes[i]} {marker}')
+    plt.scatter(feature1[i], feature2[i], marker=marker)
+
 plt.show()
 
